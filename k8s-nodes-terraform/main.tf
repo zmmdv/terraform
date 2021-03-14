0f85b57e5c8dbf7601000ad01e3c6fa35e93640b
zmmdv <66763859+zmmdv@users.noreply.github.com>
2021-03-14 12:50:58 +0400

    Update main.tf

--- k8s-nodes-terraform/main.tf
+++ k8s-nodes-terraform/main.tf
@@ -31,7 +31,7 @@ data "vsphere_virtual_machine" "template" {
 }
 
 resource "vsphere_virtual_machine" "master" {
-  name             = (var.master_node_name[count.index])
+  name             = "${var.master_node_name}${count.index}"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
   folder = var.master_nodes_folder
@@ -59,13 +59,13 @@ resource "vsphere_virtual_machine" "master" {
 
     customize {
       linux_options {
-        host_name = (var.master_node_hostname[count.index])
+        host_name = "${var.master_node_hostname}${count.index}"
         domain    = var.vm_domain
         timezone  = var.timezone
       }
 
       network_interface {
-        ipv4_address = (var.master_node_ip[count.index])
+        ipv4_address = "${var.master_node_ip}${count.index}"
         ipv4_netmask = var.masters_ipv4_netmask
       }
 
